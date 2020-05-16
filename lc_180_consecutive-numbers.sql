5f9248663a6c92ee12b018879825166cafb9b6a7
Abhishek Shah <abhishek.s@simformsolutions.com>
2020-05-17 00:35:28 +0530

    Create lc_180_consecutive-numbers.sql

---
+++ lc_180_consecutive-numbers.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select 
+    * 
+from 
+    Logs l1,
+    Logs l2,
+    Logs l3
+# result => all combination of three tables mixed => if 7 rows in table => result has 7*7*7 rows with all combinaitons
