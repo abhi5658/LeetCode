0b6ab82b373dc792ccd5f0aafc99f59ac0ed9b36
Abhishek Shah <abhishek.s@simformsolutions.com>
2020-05-17 00:48:44 +0530

    Update lc_180_consecutive-numbers.sql

--- lc_180_consecutive-numbers.sql
+++ lc_180_consecutive-numbers.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select 
+select distinct
     l1.Num AS ConsecutiveNums
 from 
     Logs l1,
@@ -10,4 +10,4 @@ where
     and l2.Id = l3.Id - 1
     and l1.Num = l2.Num
     and l2.Num = l3.Num
-# result => filter rows matching consecutive Id and same Num as well. Now selecting one of the Num row w/ columnName.
+# result => filter rows matching consecutive Id and same Num as well. Now selecting one of the Num row w/ columnName. Selecting only unique(distinct) selected numbers(a number can have more than once 3 times occuring series)
