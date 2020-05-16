109e6e99d8fa9a10dca84855303a9ca9e71fb1df
Abhishek Shah <abhishek.s@simformsolutions.com>
2020-05-16 21:40:30 +0530

    Create lc_177_db_nth-highest-salary.sql

---
+++ lc_177_db_nth-highest-salary.sql
@@ -0,0 +1,17 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+    SET N = N - 1;
+  RETURN (
+      # Write your MySQL query statement below.
+      select (
+        select distinct
+            Salary 
+          from 
+            Employee
+          order by 
+            Salary DESC
+          limit 1
+          offset N
+      )
+  );
+END
