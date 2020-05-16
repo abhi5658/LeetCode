1a2a5f0622b8d47e6e074c0b5c0ac747dc2b0067
Abhishek Shah <abhishek.s@simformsolutions.com>
2020-05-17 00:47:00 +0530

    Update lc_180_consecutive-numbers.sql

--- lc_180_consecutive-numbers.sql
+++ lc_180_consecutive-numbers.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 select 
-    * 
+    l1.Num AS ConsecutiveNums
 from 
     Logs l1,
     Logs l2,
@@ -10,4 +10,4 @@ where
     and l2.Id = l3.Id - 1
     and l1.Num = l2.Num
     and l2.Num = l3.Num
-# result => filter rows matching consecutive Id and same Num as well
+# result => filter rows matching consecutive Id and same Num as well. Now selecting one of the Num row w/ columnName.
