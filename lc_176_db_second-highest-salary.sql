b51e272e56b5bfae46619ab63d98df4d9172815e
Abhishek Shah <abhishek.s@simformsolutions.com>
2020-05-16 18:21:04 +0530

    return null when no result

--- lc_176_db_second-highest-salary.sql
+++ lc_176_db_second-highest-salary.sql
@@ -1,7 +1,11 @@
 # Write your MySQL query statement below
-select 
-Salary as SecondHighestSalary 
-from Employee 
-order by Salary DESC 
-limit 1
-offset 1;
+select(
+    select 
+        Salary 
+    from 
+        Employee 
+    order by 
+        Salary DESC 
+    limit 1 
+    offset 1
+) as SecondHighestSalary;
