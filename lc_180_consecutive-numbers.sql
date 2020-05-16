e20257183c21ca4874ce436be9de4a154bf70704
Abhishek Shah <abhishek.s@simformsolutions.com>
2020-05-17 00:44:34 +0530

    Update lc_180_consecutive-numbers.sql

--- lc_180_consecutive-numbers.sql
+++ lc_180_consecutive-numbers.sql
@@ -8,4 +8,6 @@ from
 where
     l1.Id = l2.Id - 1
     and l2.Id = l3.Id - 1
-# result => filter rows matching consecutive Ids from the joined tables e.g. Ids: [1,2,3], [2,3,4]
+    and l1.Num = l2.Num
+    and l2.Num = l3.Num
+# result => filter rows matching consecutive Id and same Num as well
