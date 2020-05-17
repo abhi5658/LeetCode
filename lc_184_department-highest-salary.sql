3b177b8cf07168d6237cb2acba4340bfdf09ff6b
Abhishek Shah <abhishekhemendrashah@gmail.com>
2020-05-18 01:01:56 +0530

    Update lc_184_department-highest-salary.sql

--- lc_184_department-highest-salary.sql
+++ lc_184_department-highest-salary.sql
@@ -24,3 +24,26 @@ inner join
 where 
     e.DepartmentId = m.dId
     and e.Salary = m.max
+
+
+# Alternate
+# Write your MySQL query statement below
+select 
+    d.Name as Department,
+    e.Name as Employee, 
+    e.Salary
+from 
+    Employee e 
+right join 
+    Department d
+on 
+    d.Id = e.DepartmentId
+where 
+    (e.DepartmentId, e.Salary) 
+in 
+    (select
+        DepartmentId, max(Salary)
+    from 
+        Employee 
+    group by 
+        DepartmentId)
