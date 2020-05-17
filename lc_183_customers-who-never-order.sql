7b1ec157b714d387dfaa021d1799650e9a5ab55a
Abhishek Shah <abhishekhemendrashah@gmail.com>
2020-05-17 19:50:56 +0530

    Create lc_183_customers-who-never-order.sql

---
+++ lc_183_customers-who-never-order.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+select 
+    Name as Customers
+from
+    Customers
+where
+    Id 
+        not in
+            (select distinct
+                CustomerId
+            from
+                Orders);
