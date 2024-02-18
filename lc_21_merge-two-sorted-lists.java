a43d3ad33fb998399438ede03d948a4f772f59e3
Abhishek Shah <myselfabhishek1997@gmail.com>
2024-02-18 05:56:42 +0530

    Update lc_21_merge-two-sorted-lists.java

--- lc_21_merge-two-sorted-lists.java
+++ lc_21_merge-two-sorted-lists.java
@@ -9,6 +9,38 @@
  * }
  */
 class Solution {
+  public ListNode mergeTwoLists(ListNode list1, ListNode list2) { // recursion
+    // if(list1 == null && list2 == null)
+    if (list1 == null)
+      return list2;
+
+    if (list2 == null)
+      return list1;
+
+    ListNode temp = null;
+
+    if (list1.val <= list2.val) {
+      temp = list1;
+      temp.next = mergeTwoLists(list1.next, list2);
+    } else {
+      temp = list2;
+      temp.next = mergeTwoLists(list1, list2.next);
+    }
+    return temp;
+  }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
   public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
     ListNode currentSorted = new ListNode(); // 4 [4] // initialise to handle list1 = [] list2 = [0]
     ListNode head = currentSorted;
