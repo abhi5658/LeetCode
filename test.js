5bcecb84e98b31d42d0f26543e2706e3edd800f1
Abhishek Shah <myselfabhishek1997@gmail.com>
2024-02-24 16:53:56 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,25 +1,35 @@
 /**
- * @param {number[]} height
+ * @param {number[]} prices
  * @return {number}
  */
-var maxArea = function (height) {
-  let maximumArea = 0;
-  let pointerA = 0;
-  let pointerB = height.length - 1
-  while (pointerA < pointerB) {
-    const lengthA = height[pointerA]
-    const lengthB = height[pointerB]
-    const area = Math.min(lengthA, lengthB) * (pointerB - pointerA)
-    if (area > maximumArea) {
-      maximumArea = area
-    }
-    if (lengthA < lengthB) {
-      pointerA++
-    } else {
-      pointerB--
+var maxProfit = function (prices) {
+  let profit = 0, //
+    buy = prices[0]; //
+  for (let i = 1; i < prices.length; i++) {
+    const previousPrice = prices[i - 1]; //
+    const currentPrice = prices[i]; //
+    if (previousPrice > currentPrice) { // p > c // peak gone
+      profit += unrealisedProfit;
+      buy = currentPrice;
+      unrealisedProfit = 0;
+    } else if (currentPrice < buy) {  // c < b - decreasing line
+      buy = currentPrice;
+    } else if (previousPrice < currentPrice) { // c > b,p // increasing line
+      unrealisedProfit = currentPrice - buy;
     }
   }
-  return maximumArea;
+  return profit + unrealisedProfit
 };
+// var maxProfit = function (prices) {
+//   // let diff = 0
+//   let maxProfit = 0
+//   for (let i = 0; i < prices.length - 1; i++) {
+//     if (prices[i + 1] - prices[i] > 0) {
+//       maxProfit += (prices[i + 1] - prices[i]);
+//     }
+//   }
+//   return maxProfit;
 
-console.log(maxArea([1, 1]));
+// };
+
+console.log(maxProfit([7, 6, 4, 3, 1]));
