573fc539407e5525b02891756f306d92894a3a49
Abhishek Shah <myselfabhishek1997@gmail.com>
2024-02-29 22:57:43 +0530

    Update lc_62_unique-paths.java

--- lc_62_unique-paths.java
+++ lc_62_unique-paths.java
@@ -7,20 +7,25 @@ class Solution {
   // 2 | 1 | 3 | 6 | 10| 15| 21| 28
   public int uniquePaths(int m, int n) {
     int[][] grid = new int[m][n];
-    grid[0][0] = 1;
+
+    grid[0][0] = 1; // initialise base case
+
     for (int i = 0; i < m; i++) {
       for (int j = 0; j < n; j++) {
-        if (i == 0 && j == 0)
+        if (i == 0 && j == 0) { // skip base case
           continue;
-        if (i == 0) {
-          grid[i][j] = grid[i][j - 1];
-        } else if (j == 0) {
-          grid[i][j] = grid[i - 1][j];
+        }
+
+        if (i == 0) { // when top is out of bounds
+          grid[i][j] = grid[i][j - 1]; // only add left
+        } else if (j == 0) { // when left is out of bounds
+          grid[i][j] = grid[i - 1][j]; // only add top
         } else {
           grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
         }
       }
     }
+
     return grid[m - 1][n - 1];
   }
 }
